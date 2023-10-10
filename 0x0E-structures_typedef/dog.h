ddc59485d67f7260e12c9f3832a3e5b6da143537
Rabbielikplim77 <rabbirage6@gmail.com>
2023-10-09 22:45:35 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog structure
+ * @name: Name
+ * @age: Age
+ * @owner: Owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
